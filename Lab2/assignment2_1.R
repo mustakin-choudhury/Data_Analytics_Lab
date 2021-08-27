f79e8f21dedd0bf0aef6af64ed8d3a54d714751d
mustakin-choudhury <78856307+mustakin-choudhury@users.noreply.github.com>
2021-08-27 14:19:56 +0530

    Add files via upload

---
+++ Lab2/assignment2_1.R
@@ -0,0 +1,41 @@
+# Get and print current working directory.
+print(getwd())
+
+# Set current working directory.
+{setwd("E:/R/dataset")
+print(getwd())
+}
+
+#1.Perform the following pre-processing tasks.
+#(a) Get the dataset and read it using read.csv function. Dataset contains four columns:
+#Country, Age, Salary, Purchased and has total 10 observations. Dataset contains
+#information of customers of some company and first three columns are information
+#of a customer like country, age, salary and fourth column tells if the customer has
+#purchased the product of the company or not. The attribute "Purchase" is the target
+#variable names.
+{data <- read.csv('E:/R/dataset/Dataset.csv')
+print(data)
+}
+
+{#(b) The dataset contains missing data.Handle the missing data by taking mean/median/mode.
+
+data$Age[is.na(data$Age)]<-mean(data$Age,na.rm=TRUE)
+print(data)
+data$Salary[is.na(data$Salary)]<-median(data$Salary,na.rm=TRUE)
+print(data)
+}
+#(c) Encode categorical data. Encode "France", "Spain" and "Germany" with 1, 2, 3
+#values. Also encode the target variable with 0 and 1 i.e. Encode Yes with 0 and No
+#with 1. factor function or if-else statement can be used to convert the categorical
+#features into numeric features.
+
+{data$Country=factor(data$Country,levels=c('France', 'Spain', 'Germany'),labels=c(1,2,3))
+print(data)
+data$Purchased=factor(data$Purchased,levels=c('Yes','No'),labels=c(0,1))
+print(data)
+}
+#(d)  Scale the dataset using any of the scaling technique. Use scale function to scale the values.
+
+dataset_matrix<-matrix(unlist(data), ncol=4)
+print("After Scaling:")
+print(scale(dataset_matrix))
